a7f50183108f34c625109fe07b24d39d9d62c65f
KHJ <080821@naver.com>
2023-04-24 00:59:04 +0900

    SessionConst 클래스 생성

--- src/main/java/hello/login/web/SessionConst.java
+++ src/main/java/hello/login/web/SessionConst.java
@@ -3,3 +3,11 @@ package hello.login.web;
 public class SessionConst {
     public static final String LOGIN_MEMBER = "loginMember";
 }
+
+
+/*
+인터페이스로 만드는게 맞음. 위에처럼 두는 이유는 이해하기 쉽게 하려고
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}
+*/
