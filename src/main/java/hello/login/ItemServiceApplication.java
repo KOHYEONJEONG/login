4e9a0ab153b98f8dd079d47515c2bcf6095144e3
KOHYEONJEONG <080821@naver.com>
2024-05-31 15:45:50 +0900

    주석 추가

--- src/main/java/hello/login/ItemServiceApplication.java
+++ src/main/java/hello/login/ItemServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ItemServiceApplication {
-
+//main
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
